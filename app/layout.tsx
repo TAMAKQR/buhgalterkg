f94a3bdd7e137393573a5e2c7425124c5b0cc61c
Buhgaler <buhgaler@telegram.app>
2026-01-04 15:31:21 +0700

    Add viewport settings to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,10 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-    themeColor: '#0f172a'
+    themeColor: '#0f172a',
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
